2df794cfc67ff2244b720bf5ed17c8c3b9daf056
Daniel Ngwasi <damnngwasi@gmail.com>
2026-05-04 22:16:40 +0300

    fix(wallets): standalone group_wallet template, remove extends base.html

--- chamapro/chamapro/settings.py
+++ chamapro/chamapro/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'chamapro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # ← change this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
