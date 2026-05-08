cce5f875d2aa186a8c45046c7e36aa612d04f602
Daniel Ngwasi <damnngwasi@gmail.com>
2026-05-08 10:56:38 +0300

    Add active_chama context processor to fix base.html sidebar on all pages

--- chamapro/chamapro/context_processors.py
+++ chamapro/chamapro/context_processors.py
@@ -1,3 +1,5 @@
+from .models import Membership
+
 def user_subscription(request):
     if request.user.is_authenticated:
         try:
@@ -6,3 +8,29 @@ def user_subscription(request):
             sub = None
         return {'user_subscription': sub}
     return {'user_subscription': None}
+
+
+def active_chama(request):
+    if not request.user.is_authenticated:
+        return {}
+
+    active_chama_id = request.session.get('active_chama_id')
+    membership = None
+
+    if active_chama_id:
+        membership = Membership.objects.filter(
+            user=request.user,
+            chama_id=active_chama_id,
+            active=True
+        ).select_related('chama').first()
+
+    if not membership:
+        membership = Membership.objects.filter(
+            user=request.user,
+            active=True
+        ).select_related('chama').first()
+
+    return {
+        'chama': membership.chama if membership else None,
+        'my_membership': membership,
+    }
